8312281b70ff839d3bdd9160eb31f15e07fa710b
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-02-07 16:44:54 +0100

    Improved graphics, edited pause mode, added audio
    The paddles now have colors, as well as the ball. In the middle of the screen you can now see a dotted line to split the two halfs of the player, like in the original Pong game. When you pause the game with space, you cannot move the paddles anymore in order to prevent cheating (pausing the game and then set the paddle to its correct position). I also added two audio file. When the ball hits a paddle or  a players scores, the corresponding sound is played.

--- MainPanel.java
+++ MainPanel.java
@@ -1,17 +1,25 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
 import java.util.Random;
+import java.awt.BasicStroke;
 
 
 public class MainPanel extends JPanel implements ActionListener, KeyListener{
 	private static final long serialVersionUID = -2324407767446391362L;
+	
+	File paddleSoundFile = new File("resources\\paddle.wav");
+	File scoreSoundFile = new File("resources\\score.wav");
 	
 	final int playerWidth = 20;
 	final int playerHeight = 150;
@@ -35,7 +43,10 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
 	public static int player1Score = 0;
 	public static int player2Score = 0;
 	
-	MainPanel(){		
+	Clip[] audioClips = new Clip[2];
+	
+	MainPanel(){	
+        audioClips = initSound();
 		this.setBounds(0,0,Frame.frameWidth, Frame.frameHeight);
 		this.setBackground(Color.black);
 		this.setLayout(null);
@@ -54,7 +65,30 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
         this.setFocusable(true);
         this.setFocusTraversalKeysEnabled(false);
         
+        
         resetAfterScore();
+	}
+	
+	private Clip[] initSound() {
+		Clip[] audioClips = new Clip[2];
+		
+		try {
+			AudioInputStream paddleAudioStream = AudioSystem.getAudioInputStream(paddleSoundFile);
+			AudioInputStream scoreAudioStream = AudioSystem.getAudioInputStream(scoreSoundFile);
+			
+			Clip paddleSoundClip = AudioSystem.getClip();
+			Clip scoreSoundClip = AudioSystem.getClip();
+			
+			paddleSoundClip.open(paddleAudioStream);
+			scoreSoundClip.open(scoreAudioStream);
+			
+			audioClips[0] = paddleSoundClip;
+			audioClips[1] = scoreSoundClip;
+		} catch(Exception e) {
+				System.out.println("Something went wrong with the sound!");
+		}
+		
+		return audioClips;
 	}
 	
 	private int getRandomSpeed() {
@@ -65,22 +99,31 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		player1UpperY += player1Velocity;
-		player2UpperY += player2Velocity;
-
-		player1LowerY = player1UpperY + playerHeight;
-		player2LowerY = player2UpperY + playerHeight;
-		
-		if(ballX < playerWidth && ballY > player1UpperY && ballY < player1LowerY) currentBallXSpeed = speed;
-		if(ballX > Frame.frameWidth - playerWidth -40 && ballY > player2UpperY && ballY < player2LowerY) currentBallXSpeed = -speed;
-		
-		if(player1UpperY < 0) player1UpperY = 0;
-		if(player1UpperY > 611) player1UpperY = 611;
-		
-		if(player2UpperY < 0) player2UpperY = 0;
-		if(player2UpperY > 611) player2UpperY = 611;
-		
 		if(isRunning) {
+
+			player1UpperY += player1Velocity;
+			player2UpperY += player2Velocity;
+	
+			player1LowerY = player1UpperY + playerHeight;
+			player2LowerY = player2UpperY + playerHeight;
+			
+			if(ballX < playerWidth && ballY > player1UpperY && ballY < player1LowerY) {
+				currentBallXSpeed = speed;
+				audioClips[0].start();
+				audioClips[0].setMicrosecondPosition(0);
+			}
+			if(ballX > Frame.frameWidth - playerWidth -40 && ballY > player2UpperY && ballY < player2LowerY) {
+				currentBallXSpeed = -speed;
+				audioClips[0].start();
+				audioClips[0].setMicrosecondPosition(0);
+			}
+			
+			if(player1UpperY < 0) player1UpperY = 0;
+			if(player1UpperY > 611) player1UpperY = 611;
+			
+			if(player2UpperY < 0) player2UpperY = 0;
+			if(player2UpperY > 611) player2UpperY = 611;
+		
 			ballX += currentBallXSpeed;
 			ballY += currentBallYSpeed;
 			
@@ -88,16 +131,19 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
 				player2Score++;
 				PlayerScore.updateGUI();
 				resetAfterScore();
+				audioClips[1].start();
+				audioClips[1].setMicrosecondPosition(0);
 			}
 			if(ballX > Frame.frameWidth) {
 				player1Score++;
 				PlayerScore.updateGUI();
 				resetAfterScore();
+				audioClips[1].start();
+				audioClips[1].setMicrosecondPosition(0);
 			}
 			
 			if(ballY < 0) currentBallYSpeed = speed;
-			if(ballY > Frame.frameHeight - 65) currentBallYSpeed = -speed;
-			
+			if(ballY > Frame.frameHeight - 65) currentBallYSpeed = -speed;		
 		}
 		repaint();
 	}
@@ -117,7 +163,7 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
 	
 	public void resetAfterScore() {
 		isRunning = false;
-		ballX = Frame.frameWidth /2;
+		ballX = Frame.frameWidth /2 -10;
 		ballY = Frame.frameHeight /2;
 		currentBallXSpeed = getRandomSpeed();
 		currentBallYSpeed = getRandomSpeed();
@@ -134,12 +180,23 @@ public class MainPanel extends JPanel implements ActionListener, KeyListener{
 
 	}
 
+	float dash[] = {20.0f};
+	BasicStroke dashed = new BasicStroke(10,BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 100.0f, dash, 0f);
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.white);
+		
+		g.setColor(Color.blue);
 		g.fillRect(0, player1UpperY, playerWidth, playerHeight);
+		g.setColor(Color.red);
 		g.fillRect(Frame.frameWidth - playerWidth -16, player2UpperY, playerWidth, playerHeight);
-		g.fillOval(ballX,ballY,ballSize,ballSize);
+		
+		g.setColor(Color.white);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(dashed);
+		g2.drawLine(Frame.frameWidth/2, 0, Frame.frameWidth/2, Frame.frameHeight);
+		
+		g.setColor(Color.yellow);
+		g.fillOval(ballX,ballY,ballSize,ballSize);	
 	}
 
 }
